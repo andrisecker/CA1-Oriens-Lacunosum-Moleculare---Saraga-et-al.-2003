d3a8024de020f708d8b0234fc35aa97a6381375f
Andris Ecker <ecker.andris@gmail.com>
2016-04-14 17:03:35 +0100

    update

--- NeuroML2/export_nml2.py
+++ NeuroML2/export_nml2.py
@@ -1,11 +1,5 @@
 from pyneuroml.neuron import export_to_neuroml2
 from pyneuroml.neuron.nrn_export_utils import clear_neuron
 
-export_to_neuroml2("../NEURON/initFig9BCase2", "OLM_Fig9BCase2_morph.cell.nml", includeBiophysicalProperties=False)
+export_to_neuroml2("../NEURON/SaragaOLM_Case2.hoc", "SaragaOLM_Case2.nml", includeBiophysicalProperties=True)
 
-'''
-clear_neuron()
-os.chdir('../NeuroML2')  
-
-export_to_neuroml2("create_cell.hoc", "../NeuroML2/OLM_Fig9BCase2.cell.nml", includeBiophysicalProperties=True)
-'''
