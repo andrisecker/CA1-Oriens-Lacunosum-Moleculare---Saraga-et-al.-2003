806381a4bc08eaa0486e8c8222fdc27b5ebad459
Andris Ecker <ecker.andris@gmail.com>
2016-04-15 11:14:03 +0100

    before merging

--- NeuroML2/export_nml2.py
+++ NeuroML2/export_nml2.py
@@ -1,5 +1,5 @@
 from pyneuroml.neuron import export_to_neuroml2
 from pyneuroml.neuron.nrn_export_utils import clear_neuron
 
-export_to_neuroml2("../NEURON/SaragaOLM_Case2.hoc", "SaragaOLM_Case2.nml", includeBiophysicalProperties=True)
+export_to_neuroml2("../NEURON/SaragaOLM_Case2.hoc", "SaragaOLM_Case2.nml", includeBiophysicalProperties=False)
 
