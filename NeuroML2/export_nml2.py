9f9f6e1078cf35be5814a25d85c6cb354c7e31b6
Padraig Gleeson <p.gleeson@gmail.com>
2016-04-15 10:49:30 +0100

    Cell file

--- NeuroML2/export_nml2.py
+++ NeuroML2/export_nml2.py
@@ -1,5 +1,8 @@
 from pyneuroml.neuron import export_to_neuroml2
 from pyneuroml.neuron.nrn_export_utils import clear_neuron
 
-export_to_neuroml2("../NEURON/SaragaOLM_Case2.hoc", "SaragaOLM_Case2.nml", includeBiophysicalProperties=True)
+export_to_neuroml2("../NEURON/tester_Case2.hoc", 
+                   "SaragaOLM_Case2.cell.nml", 
+                   includeBiophysicalProperties=True,
+                   known_rev_potentials={'IA':-100, 'Ih':-32.9})
 
